e62bdbf470a808f2db425ee27b04663dc71e329f
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2021-09-15 19:33:29 -0500

    add src/ files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", input: "" };
+  }
+  
+  myChangeHandler = (event) => {
+    this.setState({ input: event.target.value });
+  };
+  
+  handleSubmit = (e) =>{ 
+    e.preventDefault();
+    this.setState({username: this.state.input})
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Hello {this.state.username}</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+          onChange={this.myChangeHandler}
+        />
+        <button>submit</button>
+      </form>
+    );
+  }
 }
 
 export default App;
